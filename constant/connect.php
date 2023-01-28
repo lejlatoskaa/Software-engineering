dfcc7a53752adf66c48ac45383a8391d88c25441
Bl3nd865 <59256249+Bl3nd865@users.noreply.github.com>
2023-01-28 23:41:01 +0100

    Create connect.php

---
+++ constant/connect.php
@@ -0,0 +1,13 @@
+<?php
+$host     = "localhost"; 
+$username = "root"; 
+$password = ""; 
+$db_name  = ""; 
+
+
+$con = mysqli_connect($host, $username, $password, $db_name);
+
+if (!$con) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error($con);
+}
+?>
